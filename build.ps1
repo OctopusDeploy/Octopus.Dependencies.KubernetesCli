654743d228f0df5d2c8a48bb61c588b631a0d9bd
Matthew Casperson <matthewcasperson@gmail.com>
2018-03-09 07:07:58 +1000

    Removed BITS

--- build.ps1
+++ build.ps1
@@ -2,5 +2,5 @@ $versionUrl = "https://storage.googleapis.com/kubernetes-release/release/stable.
 $version = $(Invoke-WebRequest -UseBasicParsing -Uri $versionUrl).Content.Trim()
 $cleanedVersion = $version -Replace "^v",""
 $downloadLink = "https://storage.googleapis.com/kubernetes-release/release/$version/bin/windows/386/kubectl.exe"
-Start-BitsTransfer $downloadlink -DisplayName "Getting KubeCTL from $downloadlink" -Destination "."
+Invoke-WebRequest -UseBasicParsing -Uri $downloadlink -OutFile kubectl.exe
 (Get-Content kubernetes.nuspec).replace('#{KuberenetesVersion}', $cleanedVersion) | Set-Content kubernetes-processed.nuspec
