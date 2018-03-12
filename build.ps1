92db9d6663e1665b611686b2d8eb6892274a93e9
Matthew Casperson <matthewcasperson@gmail.com>
2018-03-13 07:50:37 +1000

    Adding helm to the package

--- build.ps1
+++ build.ps1
@@ -1,4 +1,10 @@
 $versionUrl = "https://storage.googleapis.com/kubernetes-release/release/stable.txt"
+$helmUrl = "https://storage.googleapis.com/kubernetes-helm/helm-v2.8.2-windows-amd64.tar.gz"
+
+Install-Package 7Zip4Powershell -Force
+Invoke-WebRequest -UseBasicParsing -Uri $helmUrl -OutFile helm.tar.gz
+Expand-7zip helm.tar.gz .
+Expand-7zip helm.tar .
 $version = $(Invoke-WebRequest -UseBasicParsing -Uri $versionUrl).Content.Trim()
 $cleanedVersion = $version -Replace "^v",""
 $downloadLink = "https://storage.googleapis.com/kubernetes-release/release/$version/bin/windows/386/kubectl.exe"
