61fc8087fce54bf24d06b549bf0f59c15edf7d99
Matthew Casperson <matthewcasperson@gmail.com>
2018-03-19 08:17:04 +1000

    Removed helm

--- README.md
+++ README.md
@@ -2,6 +2,4 @@
 
 Each run of the `build.ps1` script will download the latest stable version of `kubectl.exe`.
 
-The URL to Helm needs to be updated manually in the `build.ps1` script.
-
 The version of the resulting nuget package is based on the `kubectl.exe` version, although this may need to be changed in future.
--- build.ps1
+++ build.ps1
@@ -1,10 +1,12 @@
-$versionUrl = "https://storage.googleapis.com/kubernetes-release/release/stable.txt"
-$helmUrl = "https://storage.googleapis.com/kubernetes-helm/helm-v2.8.2-windows-amd64.tar.gz"
+# The following steps can be used to include Helm, but we are not doing this for now.
+# Including helm may or may not be something we do as a later enhancement to the kubernetes steps.
+#$helmUrl = "https://storage.googleapis.com/kubernetes-helm/helm-v2.8.2-windows-amd64.tar.gz"
+#Install-Package 7Zip4Powershell -Force
+#Invoke-WebRequest -UseBasicParsing -Uri $helmUrl -OutFile helm.tar.gz
+#Expand-7zip helm.tar.gz .
+#Expand-7zip helm.tar .
 
-Install-Package 7Zip4Powershell -Force
-Invoke-WebRequest -UseBasicParsing -Uri $helmUrl -OutFile helm.tar.gz
-Expand-7zip helm.tar.gz .
-Expand-7zip helm.tar .
+$versionUrl = "https://storage.googleapis.com/kubernetes-release/release/stable.txt"
 $version = $(Invoke-WebRequest -UseBasicParsing -Uri $versionUrl).Content.Trim()
 $cleanedVersion = $version -Replace "^v",""
 $downloadLink = "https://storage.googleapis.com/kubernetes-release/release/$version/bin/windows/386/kubectl.exe"
